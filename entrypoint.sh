ca13c32019937fcfadc7ddb449a2278ba5d123aa
dthanos <dthanos95@gmail.com>
2024-11-07 23:25:44 +0200

    minor corrections

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,10 @@ composer install
 php artisan key:generate
 php artisan config:cache
 php artisan migrate:refresh --seed
+php artisan queue:work&
 
 chmod 777 -R .
 
 service nginx restart
+
 php-fpm
